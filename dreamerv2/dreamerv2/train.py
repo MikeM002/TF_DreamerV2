cccdbbd50a68e2264654d811390514b95e30fc23
NowHere <111616872+GitterOP@users.noreply.github.com>
2025-04-01 20:09:59 -0500

    Pequeños cambios de comparativas, la base funcional sigue igual
    Solo se cambio un poco la config, pero la base es la misma

--- dreamerv2/dreamerv2/train.py
+++ dreamerv2/dreamerv2/train.py
@@ -155,7 +155,7 @@ def ensure_channel_consistency(config, train_env, eval_env):
 
 def main():
 
-  print("-"*100,"Version:",33)
+  print("-"*100,"Version:",40)
 
   #configs = yaml.safe_load((
       #pathlib.Path(sys.argv[0]).parent / 'configs.yaml').read_text())
