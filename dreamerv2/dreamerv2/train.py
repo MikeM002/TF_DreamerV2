6fdd53ef32585d4e38afac16b20b63a19c061fe7
NowHere <111616872+GitterOP@users.noreply.github.com>
2025-04-07 21:05:54 -0500

    Cambios de parametros de ejecucion y graficosv3
    Los cambios de parametros son solo nro de pasos y cosas menores

--- dreamerv2/dreamerv2/train.py
+++ dreamerv2/dreamerv2/train.py
@@ -155,7 +155,7 @@ def ensure_channel_consistency(config, train_env, eval_env):
 
 def main():
 
-  print("-"*100,"Version:",44)
+  print("-"*100,"Version:",51)
 
   #configs = yaml.safe_load((
       #pathlib.Path(sys.argv[0]).parent / 'configs.yaml').read_text())
